03b987200ece3b5376653b9c59270363a2b4afe2
moyang <3034065341@qq.com>
2025-10-26 03:10:16 +0800

    feat: 移除Vercel相关配置和无服务器函数，优化项目结构

--- api/admin/[...path].js
+++
@@ -1,8 +0,0 @@
-// Vercel serverless function for admin routes
-const serverModule = require('../../src/server.js');
-
-module.exports = async (req, res) => {
-  // Set the URL to include the admin prefix for proper routing
-  req.url = `/admin${req.url}`;
-  return serverModule(req, res);
-};
